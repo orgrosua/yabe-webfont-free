d259abfbf99c128ea54d2c0a938b54e1e0da3df6
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-12-15 14:39:40 +0000

    Yabe Webfont 1.0.60

--- constant.php
+++ constant.php
@@ -25,11 +25,11 @@ class YABE_WEBFONT
     /**
      * @var string
      */
-    public const VERSION = '1.0.59';
+    public const VERSION = '1.0.60';
     /**
      * @var int
      */
-    public const VERSION_ID = 10059;
+    public const VERSION_ID = 10060;
     /**
      * @var int
      */
@@ -41,7 +41,7 @@ class YABE_WEBFONT
     /**
      * @var int
      */
-    public const RELEASE_VERSION = 59;
+    public const RELEASE_VERSION = 60;
     /**
      * @var string
      */
